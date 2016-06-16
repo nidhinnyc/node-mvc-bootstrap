0d397f04f46cbd52cb2951a28d28af1b90281cc7
Nidhin Surendran <nidhin.surendran@thomsonreuters.com>
2016-06-16 16:53:02 -0400

    middleware addition

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ var express = require('express')
 var app = express()
 var port = process.env.PORT || 3011
 var morgan = require('morgan')
+var cookieParser = require("cookie-parser")
+var session = require("express-session")
 
 app.use(morgan('dev'))
+app.use(cookieParser())
+app.use(session({
+    secret: 'cookie_secret',
+    resave: true,
+    saveUninitialized: true
+}));
 
 app.get('/', function (req, res) {
   res.send('Hello World')
+  console.log(req.cookies)
+  console.log(req.session)
 })
  
 app.listen(port)
