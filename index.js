cb861a338bf8d60e7805ea46b40552910ddeeb29
Nidhin Surendran <nidhins@gmail.com>
2016-06-20 13:57:27 -0400

    directory changes

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-var express = require('express')
-var hbs = require('express-handlebars')
-var morgan = require('morgan')
+var express = require('express');
+var hbs = require('express-handlebars');
+var morgan = require('morgan');
+var path = require('path');
 
-var port = process.env.PORT || 3011
+var port = process.env.PORT || 3011;
 
-var app = express()
+var app = express();
 
-app.use('/incl', express.static(__dirname + '/bower_components/'));
-app.use('/public', express.static(__dirname + '/public/'));
+app.use('/incl', express.static(__dirname + '/bower_components'));
+app.use('/public', express.static(__dirname + '/public'));
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
 handlebars = hbs.create({
     defaultLayout: 'main',
     extname: '.html'
 });
 
-app.engine('html', handlebars.engine)
+app.engine('html', handlebars.engine);
 app.set('view engine', 'html');
 
-require('./app/routes.js')(app)
+require('./app/routes.js')(app);
 
-app.listen(port)
+app.listen(port);
