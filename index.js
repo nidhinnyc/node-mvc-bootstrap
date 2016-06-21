ce9fa0f8745854d93be436338bc77aa998e5f154
Nidhin Surendran <nidhins@gmail.com>
2016-06-21 11:29:26 -0400

    removed bower folder

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var hbs = require('express-handlebars');
 var morgan = require('morgan');
- 
+
 var port = process.env.PORT || 3000;
 
 var app = express();
 
-app.use('/incl', express.static('./public/bower_components'));
+app.use('/incl', express.static('./bower_components'));
 app.use('/public', express.static('./public'));
 
 app.use(morgan('dev'));
