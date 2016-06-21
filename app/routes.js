36bc347573d1552bc1f501adc5f43a852be011f7
Nidhin Surendran <nidhin.surendran@thomsonreuters.com>
2016-06-21 09:31:08 -0400

    controller implemented

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,10 @@
-var HomeController = require('./controllers/HomeController');
+var HomeController = require('../controllers/HomeController');
+var ControllerTwo = require('../controllers/ControllerTwo');
 
 module.exports = function(app){
     app.get('/', HomeController.index);
-    app.get('/methodone', HomeController.methodone);
-    app.get('/methodone', HomeController.methodtwo);
+    app.get('/home/index', HomeController.index);
+    app.get('/home/methodone', HomeController.methodone);
+    app.get('/home/methodtwo', HomeController.methodtwo);
+    app.get('/two/index', ControllerTwo.index);
 }
--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var hbs = require('express-handlebars');
 var morgan = require('morgan');
-
+ 
 var port = process.env.PORT || 3000;
 
 var app = express();
 
-app.use('/incl', express.static(__dirname + '/bower_components'));
-app.use('/public', express.static(__dirname + '/public'));
+app.use('/incl', express.static('./public/bower_components'));
+app.use('/public', express.static('./public'));
 
 app.use(morgan('dev'));
 
